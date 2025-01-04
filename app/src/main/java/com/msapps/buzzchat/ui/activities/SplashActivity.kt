0a55d905cdf3cc87bd947d6d821ff595c4a6a193
Shay Shimoni <shay.shimoni@msapps.mobi>
2025-01-04 16:26:55 +0200

    chore: Move fragment/activity into ui directory

--- app/src/main/java/com/msapps/buzzchat/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/msapps/buzzchat/ui/activities/SplashActivity.kt
@@ -1,10 +1,11 @@
-package com.msapps.buzzchat
+package com.msapps.buzzchat.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.msapps.buzzchat.R
 import kotlinx.coroutines.Runnable
 
 class SplashActivity: AppCompatActivity() {
